15378c5ecc7b290795dca2cea991aeb54de4786f
itsmerahmat <rahmatgamming1@gmail.com>
2025-02-17 19:20:29 +0800

    Feat: Add new SVG icons for various functionalities

--- src/components/section/navbar.tsx
+++ src/components/section/navbar.tsx
@@ -6,7 +6,7 @@ export default function NavbarSection() {
       <nav className="navbar max-w-6xl mx-auto py-10 px-5 md:px-8 xl:px-0">
         <div className="flex flex-row items-center justify-between">
           <Image
-            src="/assets/logo-light.svg"
+            src="/svgs/logo-light.svg"
             className="h-[42px]"
             alt="logo"
             width={180}
@@ -42,45 +42,7 @@ export default function NavbarSection() {
           <div className="flex flex-row gap-x-4">
             <div className="bg-white flex items-center p-[10px] rounded-full">
               <a href="#">
-                <svg
-                  width="25"
-                  height="24"
-                  viewBox="0 0 25 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M8 7.67001V6.70001C8 4.45001 9.81 2.24001 12.06 2.03001C14.74 1.77001 17 3.88001 17 6.51001V7.89001"
-                    stroke="#080C2E"
-                    strokeWidth="2"
-                    strokeMiterlimit="10"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M9.50001 22H15.5C19.52 22 20.24 20.39 20.45 18.43L21.2 12.43C21.47 9.99 20.77 8 16.5 8H8.50001C4.23001 8 3.53001 9.99 3.80001 12.43L4.55001 18.43C4.76001 20.39 5.48001 22 9.50001 22Z"
-                    stroke="#080C2E"
-                    strokeWidth="2"
-                    strokeMiterlimit="10"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M15.9955 12H16.0045"
-                    stroke="#080C2E"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M8.99451 12H9.00349"
-                    stroke="#080C2E"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <circle cx="19.5" cy="7" r="4" fill="#FF3232" />
-                </svg>
+                <Image src="/svgs/bag-2.svg" width={25} height={25} alt="bag" />
               </a>
             </div>
             <a
